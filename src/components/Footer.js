464f0e84b106e845534813b45378550f641814ac
bobur <bobur02k@gmail.com>
2022-06-10 20:40:30 +0500

    homework

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
+
+
 function Footer() {
+    const d = new Date()
+    const year = d.getFullYear()
     return (
      <div className="footer" style={{ position: "absolute", bottom: 0, left: "600px"}}>
-         <p>copyright Reserved 2022</p>
+         <p>copyright Reserved {year}</p>
      </div>
     );
 }
